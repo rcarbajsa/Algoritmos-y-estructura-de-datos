a6003602b37983d9f8f360cfbb8bbcfeb7d47816
Max Garcia <maximogarciamtnez@gmail.com>
2017-09-22 13:37:21 +0200

    reestructura

---
+++ practica2/src/CalificacionAlreadyExistsException.java
@@ -0,0 +1,10 @@
+package aed.actasnotas;
+
+/**
+ * Exception raised by when attempting to add a new calificacion
+ * with a matricula, and there already exists another calificacion with
+ * that matricula in the database.
+ */
+public class CalificacionAlreadyExistsException extends Exception {
+}
+
---
+++ practica2/src/InvalidMatriculaException.java
@@ -0,0 +1,8 @@
+package aed.actasnotas;
+
+/**
+ * Exception raised by various methods when there is no 
+ * calificacion for a given matricula.
+ */
+public class InvalidMatriculaException extends Exception {
+}
