a8d89404b22ca91f50e5a43b5c8b8721868dd416
onmax <maximogarciamtnez@gmail.com>
2017-11-25 19:24:56 +0100

    Terminado

--- practica6/src/aed/huffman/Huffman.java
+++ practica6/src/aed/huffman/Huffman.java
@@ -1,58 +1,48 @@
 package aed.huffman;
 
-
 import es.upm.aedlib.Position;
 import es.upm.aedlib.tree.*;
-
 
 /**
  * Defines metodos for Huffman encoding of text strings.
  */
 public class Huffman {
-    private LinkedBinaryTree<Character> huffmanTree;
-    
-    public Huffman(LinkedBinaryTree<Character> huffmanTree) {
-      // NO CAMBIA ESTE METODO!!! Esta usado durante las pruebas
-      this.huffmanTree = huffmanTree;
-    }
+	private LinkedBinaryTree<Character> huffmanTree;
 
-    /**
-     * Creates a Huffman tree (and stores it in the attribute huffmanTree).
-     * The shape of the (binary) tree is
-     * defined by the array of char-codes.
-     */
-    public Huffman(CharCode[] paths) {
+	public Huffman(LinkedBinaryTree<Character> huffmanTree) {
+		this.huffmanTree = huffmanTree;
+	}
+
+	public Huffman(CharCode[] paths) {
 		this.huffmanTree = new LinkedBinaryTree<Character>();
-		huffmanTree.addRoot((char)32);
-		for(int i =0; i<paths.length;i++){
-			String cursor=paths[i].getCode();
-			Character letra=paths[i].getCh();
-			Position<Character> pos=huffmanTree.root();
-			if(cursor.length()==1){
-				Position<Character> raiz=huffmanTree.root();
-				if(cursor.charAt(0)=='1'){
+		huffmanTree.addRoot((char) 32);
+		for (int i = 0; i < paths.length; i++) {
+			String cursor = paths[i].getCode();
+			Character letra = paths[i].getCh();
+			Position<Character> pos = huffmanTree.root();
+			if (cursor.length() == 1) {
+				Position<Character> raiz = huffmanTree.root();
+				if (cursor.charAt(0) == '1') {
 					huffmanTree.insertRight(raiz, letra);
-				}else{
+				} else {
 					huffmanTree.insertLeft(raiz, letra);
 				}
-			}else{
-				for(int j=0;j<cursor.length();j++){
-					if(cursor.charAt(j)=='1'){
-						if(!huffmanTree.hasRight(pos)){
-							huffmanTree.insertRight(pos, (char)32);
+			} else {
+				for (int j = 0; j < cursor.length(); j++) {
+					if (cursor.charAt(j) == '1') {
+						if (!huffmanTree.hasRight(pos)) {
+							huffmanTree.insertRight(pos, (char) 32);
 						}
-						pos=huffmanTree.right(pos);
-						if(j+1==cursor.length()){
+						pos = huffmanTree.right(pos);
+						if (j + 1 == cursor.length()) {
 							huffmanTree.set(pos, letra);
 						}
-
-					}
-					else{
-						if(!huffmanTree.hasLeft(pos)){
-							huffmanTree.insertLeft(pos, (char)32);
+					} else {
+						if (!huffmanTree.hasLeft(pos)) {
+							huffmanTree.insertLeft(pos, (char) 32);
 						}
-						pos=huffmanTree.left(pos);
-						if(j+1==cursor.length()){
+						pos = huffmanTree.left(pos);
+						if (j + 1 == cursor.length()) {
 							huffmanTree.set(pos, letra);
 						}
 					}
@@ -61,117 +51,55 @@ public class Huffman {
 		}
 	}
 
-    //////////////////////////////////////////////////////////////////////
+	public String encode(String text) {
+		String encodeStr = "";
+		for (int i = 0; i < text.length(); i++) {
+			encodeStr += findCharacterCode(text.charAt(i), huffmanTree, huffmanTree.root());
+		}
+		return encodeStr;
+	}
 
-
-    /**
-     * Huffman encodes a text, returning a new text string
-     * containing only characters '0' and '1'.
-     */
-    public String encode(String text) {
-	// CAMBIA
-	// llama el metodo findCharacterCode
-    	
-    	String encodeStr = "";
-    	for(int i = 0; i<text.length();i++) {
-    		System.out.println("____");
-    		encodeStr += findCharacterCode(text.charAt(i),huffmanTree,huffmanTree.root());
-    	}
-    	return encodeStr;
-    }
-
-    // CAMBIA e UTILIZA si quiereis
-    private String findCharacterCode(Character ch, 
-			     BinaryTree<Character> tree,
-			     Position<Character> pos) {
-    	/*if(tree.isInternal(pos)) {
-    		if(tree.left(pos).element().equals(ch)) {
-    			path="0"+path;
-    			return path;
-    		}else if(tree.right(pos).element().equals(ch)) {
-    			path="1"+path;
-    			return path;
-    		}else {
-    			if(tree.right(pos).element().equals(ch)) {
-    				return path + "1";
-    			}
-    			if(tree.left(pos).element().equals(ch)) {
-    				return path + "0";
-    			}
-    			
-    			if(tree.right(pos).element().equals((char)32)) {
-    				pos=tree.right(pos);
-    				path = "1" + findCharacterCode(ch, tree, pos, path);
-    			}else if(tree.left(pos).element().equals((char)32)) {
-    				pos=tree.left(pos);
-    				path = "0" + findCharacterCode(ch, tree, pos, path);
-    			}else {
-    				pos=tree.root();
-    	    		pos=tree.left(pos);
-    	    		path = "0";
-    	    		System.out.println(tree.left(pos).element());
-    	    		System.out.println("ssll");
-    	    		findCharacterCode(ch, tree, pos, path);
-    			}
-    			return path;
-    		}
-    	}else {
-    		System.out.println("oo");
-    		pos=tree.root();
-    		pos=tree.left(pos);
-    		findCharacterCode(ch, tree, pos, path);
-    	}
-    	return path;*/
-    	String line = "";
-    	for (Position<Character> v : tree.children(pos)) {
+	private String findCharacterCode(Character ch, BinaryTree<Character> tree, Position<Character> pos) {
+		String line = "";
+		for (Position<Character> v : tree.children(pos)) {
 			if (v.element().equals(ch)) {
-				while(!tree.isRoot(v)) {
-					v=tree.parent(v);
-					System.out.println(tree.left(v).element());
-					if(tree.left(v).element().equals(ch)) {
-						line="0"+line;
-					}else {
-						line="1"+line;
+				while (!tree.isRoot(v)) {
+					Position<Character> w = tree.parent(v);
+					if (tree.left(w).equals(v)) {
+						line = "0" + line;
+					} else {
+						line = "1" + line;
 					}
+					v = w;
 				}
 				return line;
-				
 			} else if (tree.isInternal(v)) {
-				return findCharacterCode(ch, tree, v);
+				line += findCharacterCode(ch, tree, v);
+			} else {
+				pos = tree.root();
 			}
 		}
-    	return line;
-    }
+		return line;
+	}
 
-
-    //////////////////////////////////////////////////////////////////////
-
-    /**
-     * Given the Huffman encoded text argument (a string of only
-     * '0' and '1's), returns the corresponding normal text.
-     */
-    public String decode(String codigo) {
-		String res="";
-		Position<Character> cursor=huffmanTree.root();
-		for(int i=0;i<codigo.length();i++) {
-			if(codigo.charAt(i)=='1') {
-				cursor=huffmanTree.right(cursor);
-				if(huffmanTree.isExternal(cursor)) {
-					res+=cursor.element();
-					cursor=huffmanTree.root();
+	public String decode(String codigo) {
+		String res = "";
+		Position<Character> cursor = huffmanTree.root();
+		for (int i = 0; i < codigo.length(); i++) {
+			if (codigo.charAt(i) == '1') {
+				cursor = huffmanTree.right(cursor);
+				if (huffmanTree.isExternal(cursor)) {
+					res += cursor.element();
+					cursor = huffmanTree.root();
 				}
-			}
-			else {
-				cursor=huffmanTree.left(cursor);
-				if(huffmanTree.isExternal(cursor)) {
-					res+=cursor.element();
-					cursor=huffmanTree.root();
+			} else {
+				cursor = huffmanTree.left(cursor);
+				if (huffmanTree.isExternal(cursor)) {
+					res += cursor.element();
+					cursor = huffmanTree.root();
 				}
 			}
 		}
 		return res;
 	}
 }
-
-
-
